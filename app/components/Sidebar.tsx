97389097ead4465b649bb2b73aafc2b3ad36c32a
Faizan <fa147672@gmail.com>
2023-06-19 12:32:58 +0500

    components

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -5,14 +5,16 @@ import { useMemo } from "react"
 import { HiHome } from "react-icons/hi"
 import { BiSearch } from "react-icons/bi"
 import Box from "./Box"
+import SidebarItems from "./SidebarItems"
+import Library from "./Library"
 
 interface sidebarprops {
     children: React.ReactNode
 }
 
-const Sidebar: React.FC<sidebarprops> = ({ 
+const Sidebar: React.FC<sidebarprops> = ({
     children,
-   })=> {
+}) => {
     const pathname = usePathname()
     const routes = useMemo(() => [
 
@@ -29,7 +31,7 @@ const Sidebar: React.FC<sidebarprops> = ({
             href: "/search"
         }
 
-    ], [])
+    ], [pathname])
     return (
 
         <div className={twMerge(`
@@ -37,24 +39,35 @@ const Sidebar: React.FC<sidebarprops> = ({
             h-full
             `,
 
-          )}
+        )}
         >
-          <div 
-            className="
-              hidden 
-              md:flex 
-              flex-col 
-              gap-y-2 
-              bg-black 
-              h-full 
-              w-[300px] 
-              p-2
-            "
-          >
+            <div
+                className="
+            hidden 
+            md:flex 
+            flex-col 
+            gap-y-2 
+            bg-black 
+            h-full 
+            w-[300px] 
+            p-2
+          "
+            >
                 <Box>
-                    Sidebar!sda
+                    <div className="flex flex-col gap-y-4 px-5 py-4">
+                        {routes.map((item) => (
+                            <SidebarItems key={item.label} {...item} />
+                        ))}
+                    </div>
+                </Box>
+
+                <Box className="overflow-y-auto h-full">
+                    <Library />
                 </Box>
             </div>
+            <main className="h-full flex-1 overflow-y-auto py-2">
+                {children}
+            </main>
         </div>
 
     )
